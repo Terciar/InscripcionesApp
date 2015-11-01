35731671aad7720c1af32ab616dd9ac16b75955b
Matias M <matiasmasca@gmail.com>
2015-11-01 11:22:05 -0300

    Alumno por Colono

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,14 @@
 module ApplicationHelper
-  def badge_registrations(count)
+  def badge_registrations(count, max)
     case count
-    when 0..5
+    when 0
+     @content = tag(:span, class: "badge-default")
+    when 1..4
      @content = tag(:span, class: "badge alert-warning")
-    when 5..25
+    when 5..max
       #verde
       @content = tag(:span, class: "badge alert-success")
-    when 25..50
+    when (max+1)..50
       #verde
       @content = tag(:span, class: "badge alert-danger")
     else
