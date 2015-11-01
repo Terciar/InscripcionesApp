278da130b491503f3e7957d930b0702047d2ff9c
Matias M <matiasmasca@gmail.com>
2015-10-31 23:00:57 -0300

    Mover alumno de grupo

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -7,5 +7,11 @@ module RegistrationsHelper
     end
   end
 
-
+  ### Grupos Temporada.
+  def grupos_temporada
+    inicio_temporada = Date.new(2015,10,01)
+    fin_temporada = Date.new(2016,03,31)
+    grupos = Group.where(created_at: inicio_temporada..fin_temporada)
+    #logger.debug ("ACA!!!!!!!!!!!!!grupos: #{grupos.inspect}")
+  end
 end
