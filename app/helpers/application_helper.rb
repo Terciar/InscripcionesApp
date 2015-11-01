29a83fb362910a707e20af8eb5170ebf6cd6d8de
Matias M <matiasmasca@gmail.com>
2015-11-01 11:01:27 -0300

    Agregando badges al listado grupos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def badge_registrations(count)
+    case count
+    when 0..5
+     @content = tag(:span, class: "badge alert-warning")
+    when 5..25
+      #verde
+      @content = tag(:span, class: "badge alert-success")
+    when 25..50
+      #verde
+      @content = tag(:span, class: "badge alert-danger")
+    else
+      #default
+      @content = tag(:span, class: "badge")
+    end
+  end
 end
