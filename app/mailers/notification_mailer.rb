224af5f742b264ce725495641c994fa0acc8a95c
Matias M <matiasmasca@gmail.com>
2015-11-03 23:25:54 -0300

    Agregando Gretel

---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,12 @@
+class NotificationMailer < ApplicationMailer
+  default from: 'contacto@terciar.ga'
+
+  def new_notification_email(project)
+    @customer = User.find(project.user_id)
+    @project = project
+    @notification = @project.notifications.last
+    @url  = "http://terciar-novedades.herokuapp.com/projects/#{@project.id}/notifications/#{@notification.id}"
+    mail(to: @customer.email, subject: 'Tiene una novedad en su Proyecto')
+  end
+
+end
