4f1318b0ae615198624cfb2f68892740bd3ed2a3
Matias M <matiasmasca@gmail.com>
2015-11-21 17:27:10 -0300

    Cukes: aceptar o rechazar inscripcion

--- features/aceptar_rechazar_inscripcion_colono.feature
+++ features/aceptar_rechazar_inscripcion_colono.feature
@@ -6,14 +6,19 @@ Característica: Aceptar inscripción.
 
 Antecedentes:
   Dado que estoy identificado como "Administrador"
-  Y que existe el colono "Juan Pérez" con DNI "44123321"
   Y que existe el colono "Juan Jacinto Perez" con DNI "45123321"
   Y que existe el colono "Laura Villanueva" con DNI "42205204"
   Y estoy en la pantalla "Colonos"    
 
 #Camino Feliz
-Escenario:
+Escenario: aceptar colono
   Dado que presiono el botón "Confirmar" en el renglón del colono "Juan Jacinto Perez"
   Entonces veo un mensaje "Ha sido CONFIRMADA la inscripción de "Juan Jacinto Perez"."
-  Entonces el botón cambia a la palabra "aceptado"
-  Y el estado del colono "Juan Jacinto Perez" cambia a "aceptado"
+  Entonces el botón cambia a la palabra "Aceptado"
+  Y el estado del colono "Juan Jacinto Perez" cambia a "Aceptado"
+
+Escenario: rechazar colono
+  Dado que presiono el botón "Rechazar" en el renglón del colono "Juan Jacinto Perez"
+  Entonces veo un mensaje "Ha sido RECHAZADA la inscripción de "Juan Jacinto Perez"."
+  Entonces el botón cambia a la palabra "Rechazado"
+  Y el estado del colono "Juan Jacinto Perez" cambia a "Rechazado"
--- features/step_definitions/aceptar_rechzar_inscripcion_steps.rb
+++ features/step_definitions/aceptar_rechzar_inscripcion_steps.rb
@@ -6,10 +6,9 @@ Dado(/^que presiono el botón "(.*?)" en el renglón del colono "(.*?)"$/) do |b
     row = page.find('tr', :text => "#{nombre_colono}")
     if boton == "Confirmar"
       row.find("a.btn-success:nth-child(1)").click
-      # a:nth-child(1)
-      #registrations-list > tbody:nth-child(2) > tr:nth-child(3) > td:nth-child(13) > a:nth-child(1)
     elsif boton == "Rechazar"
-      row.find("a.btn-danger:nth-child(1)").click
+      row.find("a.btn-danger:nth-child(2)").click
+      #registrations-list > tbody:nth-child(2) > tr:nth-child(3) > td:nth-child(13) > a:nth-child(2)
     end
   end
 end
@@ -17,19 +16,23 @@ end
 Entonces(/^el botón cambia a la palabra "(.*?)"$/) do |palabra|
   #save_and_open_page
   within_table('registrations-list') do
-    page.has_content?("#{@nombre_colono}")
-    row = page.find('tr', :text => "#{@nombre_colono}")
-    row.has_content?("Aceptado")
- end
+      page.has_content?("#{@nombre_colono}")
+      row = page.find('tr', :text => "#{@nombre_colono}")
+
+    case palabra
+    when "Aceptado"
+        row.has_content?("Aceptado")
+    when "Rechazado"
+        row.has_content?("Rechazado")
+    end
+  end
 end
 
-Entonces(/^el estado del colono cambia a "(.*?)"$/) do |estado|
-  #status= 2 aprobado
-  #status = 3 rechazado
-
-end
 
 Entonces(/^el estado del colono "(.*?)" cambia a "(.*?)"$/) do |nombre_colono, estado|
+  #status= 2 aprobado
+  #status = 3 rechazado
   colono = Registration.find_by_colonist_name(nombre_colono)
-  assert colono.status, 2
+  assert colono.status, 2 if estado == "Aceptado"
+  assert colono.status, 3 if estado == "Rechazado"
 end
